fd12745e02a41aba553785837386281d565f9f17
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-26 18:47:02 +0100

    Fix: GET info request now works with mock data

--- controllers/common.js
+++ controllers/common.js
@@ -1,11 +1,11 @@
 const mock = require('../mock/mocks');
+
 module.exports = function getInfo (ctx) {
-  console.log('body', ctx.headers['type']);
   if (ctx.headers['type'] === 'user@user.com') {
     ctx.status = 200
-    return ctx.body = mock.user
+    return ctx.body = mock.user()
   } else if (ctx.headers['type'] === 'admin@admin.com'){
     ctx.status = 200
-    return ctx.body = mock.company
+    return ctx.body = mock.company()
   }
 }
