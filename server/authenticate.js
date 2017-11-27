6a9d0be620cb13b7337bf62f6f8edfc79b5c64e4
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-27 20:53:32 +0100

    Chore: add decryption of the header in login

--- server/authenticate.js
+++ server/authenticate.js
@@ -3,16 +3,21 @@ const mongoose = require('mongoose');
 const Schemas = require('../models/schemas');
 const Company = mongoose.model('Companies', Schemas.AdminSchema);
 const User = mongoose.model('Users', Schemas.UserSchema);
+const bcrypt = require('bcrypt');
+const atob = require('atob');
 
-module.exports = function (ctx) {
-    Company.findOne({name: 'Hills LLC'}, 'password', function (err, person) {
-      if (person.password === ctx.headers.password) {
+module.exports = async function (ctx) {
+    const userpass = (ctx.header.authorization.slice(6, ctx.header.authorization.length-2));
+    const username = atob(userpass).split(':')[0];
+    const password = atob(userpass).split(':')[1];
+    await Company.findOne({name: username}, 'password', function (err, person) {
+      const checkPass = bcrypt.compare(password, person.password)
+      if (checkPass) {
         ctx.body = {
-          token: jwt.sign({password: ctx.headers.password }, 'xxx'), //Should be the same secret key as the one used is ./jwt.js
-          message: "Successfully logged in!"
+          token: jwt.sign({password: ctx.headers.password }, 'xxx')
         };
         ctx.status = 200;
       }
     })
-  return ctx; 
+  return ctx;
 }
