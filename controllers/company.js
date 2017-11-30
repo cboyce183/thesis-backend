79c7e986eb3582c738236927eb0d055fa59f5fb8
Pier Roberto Lucisano <pierroberto.lucisano@gmail.com>
2017-11-30 10:20:07 +0100

    Chore: add addProduct function in controllers

--- controllers/company.js
+++ controllers/company.js
@@ -1,7 +1,7 @@
 const newCompany = require('../models/insertdata')
-const randomCompany = require ('../mock/mocks');
-
-module.exports = async function add (ctx) {
+const randomCompany = require('../mock/mocks');
+const adminPrivilege = require('../server/auth/usertype');
+async function add (ctx) {
   const data = await newCompany.addCompany(ctx.request.body) //ctx.request.body
   if (data)
     ctx.status = 201;
@@ -9,4 +9,13 @@ module.exports = async function add (ctx) {
     ctx.body = 'This company already exists';
     ctx.status = 204; //Need to check which status set in this scenario
   }
+}
+
+async function addProduct (ctx) {
+
+}
+
+module.exports = {
+  add : add,
+  addProduct: addProduct
 }
--- server/router.js
+++ server/router.js
@@ -2,7 +2,7 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router');
 const router = new Router();
-const addCompany = require('../controllers/company');
+const company = require('../controllers/company');
 const user = require('../controllers/user');
 const authenticate = require('./auth/authenticate');
 const wallet = require('../controllers/wallet');
@@ -10,9 +10,10 @@ const getInfo = require('../controllers/common');
 const jwt = require('./auth/jwt');
 
 // POST requests
-router.post('/add-company', addCompany);
+router.post('/add-company', company.add);
 router.post('/add-user', user.add);
-router.post('/signup-user', user.signup);
+router.post('/signup-user', user.signup); //It has to be a put request, has to be reviewed
+router.post('/add-product', company.addProduct);
 
 // GET requests
 router.get('/info', getInfo.getInfo); //This is meant for testing, ignore it
@@ -22,7 +23,7 @@ router.get('/login', async (next) => {
 
 // PUT requests
 
-router.put('/edit-user', user.edit) //Working on it (you better - Charlie)
+router.put('/edit-user', user.edit)
 router.put('/admin-tip', wallet.tipUser);
 router.put('/transfer', wallet.transferFunds);
 router.put('/admin-fund', wallet.addFunds);
